cc56c776a030dfc89cb88eb25b5e6fe634518337
unknown <>
2016-07-05 23:31:16 +0800

    虚拟机栈和本地方法栈

--- src/main/java/com/xhtc/Chapter2/JavaVMStackSOF.java
+++ src/main/java/com/xhtc/Chapter2/JavaVMStackSOF.java
@@ -2,7 +2,26 @@ package com.xhtc.Chapter2;
 
 /**
  * Created by Zetrov on 2016/7/5.
+ *
+ * VM Options: -Xss128k
+ *
  */
 public class JavaVMStackSOF {
 
+    private int stackLength = 1;
+
+    public void stackLeak(){
+        stackLength ++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) throws Throwable{
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length: " + oom.stackLength);
+            throw e;
+        }
+    }
 }
